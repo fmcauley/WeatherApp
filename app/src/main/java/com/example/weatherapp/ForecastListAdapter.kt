9f997aab61417ae6efacc2e2fe5a6ef26affc139
McAuley, Frank <Frank.McAuley@alliancedata.com>
2020-02-07 13:39:24 -0500

    FM - Adding the RecyclerView Logic. Not printing to screen

---
+++ app/src/main/java/com/example/weatherapp/ForecastListAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapp
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ForecastListAdapter(private val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(TextView(parent.context))
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
